cae445781b3f67f2c0b553a4c806ce7c127c9264
Vidan Lugonja <vidan.lugonja@gmail.com>
2021-10-27 22:02:36 +0200

    Add files via upload

---
+++ domaci_p3_z1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace domaci_p3_z1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 50;
+            int B = 100;
+
+            for (int i = A; i <= B; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"Broj {i} je paran broj");
+                }
+            }
+        }
+    }
+}
